c33a58b78e23897365b7a384eb10f9479a0eb7ed
ebushland <ebushland@gmail.com>
2026-09-20 13:43:11 -0500

    Week 8 Lab 7
    Code and images for Lab 7.

---
+++ Week08/Labs/Lab7RasterAnalysisProject.py
@@ -0,0 +1,26 @@
+import arcpy
+
+# Allows overwriting of existing files
+arcpy.env.overwriteOutput = True
+
+# Assign bands
+source = r"C:\Users\ebush\Desktop\TexasAM\GEOG-676\Week8"
+band1 = arcpy.sa.Raster(source + r"\band1.tif") #blue
+band2 = arcpy.sa.Raster(source + r"\band2.tif") #green
+band3 = arcpy.sa.Raster(source + r"\band3.tif") #red
+band4 = arcpy.sa.Raster(source + r"\band4.tif") #nir
+combined = arcpy.CompositeBands_management([band1, band2, band3, band4], source + r"\output_combined.tif")
+
+# Hillshade
+azimuth = 315
+altitude = 45
+shadows = "NO_SHADOWS"
+z_factor = 1
+arcpy.ddd.HillShade(source + r"\DEM.tif", source + r"\output_hillshade.tif", azimuth, altitude, shadows, z_factor)
+
+# Slope
+output_measurement = "DEGREE"
+z_factor = 1
+arcpy.ddd.Slope(source + r"\DEM.tif", source + r"\output_slope.tif", output_measurement, z_factor)
+
+print("Success!")
